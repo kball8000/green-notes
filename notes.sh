5be6f33ae8250fe529dce1ef735aa06a8ada2d15
Kevin Ball <kball8000@gmail.com>
2017-06-17 06:30:38 -0500

    Delete notes.sh

--- notes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo Running app engine server...
-#dev_appserver.py --port 8001 --admin_port 8003 -A kball-test-tools app.yaml worker.yaml
-dev_appserver.py --port 8001 --admin_port 8003 -A green-kball-notes app.yaml
